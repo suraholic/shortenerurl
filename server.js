48dbf9bac7f120bee7da6b6c1eca540c1c253a23
suraholic <suraholic@gmail.com>
2017-08-30 19:02:45 +0900

    morgan & express-basic-auth install

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
+const basicAuth = require('express-basic-auth')
 
 const app = express()
 
+const authMiddleware = basicAuth({
+  users: { 'admin': '1234' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+})
+
 app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
+app.use(morgan('tiny'))
 
-app.get('/', (req,res)=>{
+app.get('/', authMiddleware, (req,res)=>{
   res.render('index.ejs');
 })
 
